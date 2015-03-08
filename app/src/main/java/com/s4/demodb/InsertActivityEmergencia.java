808f73f354a886a6aa2a6fd6d7fd715a8606c349
eddos <ejp_45@hotmail.com>
2015-03-07 20:19:03 -0500

    Modificaciones Borja

--- app/src/main/java/com/s4/demodb/InsertActivityEmergencia.java
+++ app/src/main/java/com/s4/demodb/InsertActivityEmergencia.java
@@ -8,6 +8,7 @@ import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.RadioGroup;
 import android.widget.RadioButton;
 import android.widget.Toast;
 
@@ -46,21 +47,71 @@ public class InsertActivityEmergencia extends ActionBarActivity {
     private void insertarRegistro() {
 
         try {
+            RadioGroup grupoMedioInformacion = (RadioGroup) findViewById(R.id.medioInformacion);
+            RadioGroup grupoMedioConfirmacion = (RadioGroup) findViewById(R.id.medioConfirmacion);
+            RadioGroup grupoClaseInmueble = (RadioGroup) findViewById(R.id.claseInmueble);
+            RadioButton radioMedioInformacion = (RadioButton) findViewById(grupoMedioInformacion.getCheckedRadioButtonId());
+            RadioButton radioMedioConfirmacion = (RadioButton) findViewById(grupoMedioConfirmacion.getCheckedRadioButtonId());
+            RadioButton radioClaseInmueble = (RadioButton) findViewById(grupoClaseInmueble.getCheckedRadioButtonId());
+
             String fecha = ((EditText)findViewById(R.id.campofecha)).getText().toString();
             String hora = ((EditText)findViewById(R.id.campoHora)).getText().toString();
             String informante = ((EditText)findViewById(R.id.campoInformante)).getText().toString();
             Integer informante2 = Integer.parseInt(informante);
             String informacion_recibida = ((EditText)findViewById(R.id.campoInformacionr)).getText().toString();
-            String medio_informacion = ((RadioButton)findViewById(R.id.medioInformacion)).getText().toString();
-            Integer medio_informacion2 =Integer.parseInt(medio_informacion);
+            String medio_informacion = radioMedioInformacion.getText().toString();
+            Integer medio_informacion2= 0;
+            if (medio_informacion.equals("Personalmente"))
+            {
+                medio_informacion2=0;
+            }
+            else
+            if (medio_informacion.equals("Teléfono"))
+            {
+                   medio_informacion2=1;
+            }
+            else
+            if (medio_informacion.equals("Otros"))
+            {
+                medio_informacion2=2;
+            }
             String otro_medio_informacion = ((EditText)findViewById(R.id.campoOtro_medio)).getText().toString();
             String persona_confirmacion = ((EditText)findViewById(R.id.campoPersona_confirmacion)).getText().toString();
-            String medio_confirmacion = ((EditText)findViewById(R.id.medioConfirmacion)).getText().toString();
-            Integer medio_confirmacion2 = Integer.parseInt(medio_confirmacion);
+            String medio_confirmacion = radioMedioConfirmacion.getText().toString();
+            Integer medio_confirmacion2 = 0;
+            if (medio_confirmacion.equals("Personalmente"))
+            {
+                medio_confirmacion2=0;
+            }
+            else
+            if (medio_confirmacion.equals("Teléfono"))
+            {
+                medio_confirmacion2=1;
+            }
+            else
+            if (medio_confirmacion.equals("Otros"))
+            {
+                medio_confirmacion2=2;
+            }
+            System.out.println("El medio de Confirmacion fue = "+medio_confirmacion2);
             String descripcion_otro_medio = ((EditText)findViewById(R.id.campoMedio_confirmacion)).getText().toString();
             String direccion = ((EditText)findViewById(R.id.campoDireccion)).getText().toString();
-            String inmueble_clase = ((EditText)findViewById(R.id.campoClase_inmueble)).getText().toString();
-            Integer inmueble_clase2 = Integer.parseInt(inmueble_clase);
+            String inmueble_clase = radioClaseInmueble.getText().toString();
+            Integer inmueble_clase2 = 0;
+            if (inmueble_clase.equals("Residencial"))
+            {
+                inmueble_clase2=0;
+            }
+            else
+            if (inmueble_clase.equals("Comercial"))
+            {
+                inmueble_clase2=1;
+            }
+            else
+            if (inmueble_clase.equals("Otros"))
+            {
+                inmueble_clase2=2;
+            }
             String inmueble_propietario = ((EditText)findViewById(R.id.campoPropietario_inmmueble)).getText().toString();
             String inmueble_administrador = ((EditText)findViewById(R.id.campoInmueble_administrador)).getText().toString();
             String inmueble_arrendatario = ((EditText)findViewById(R.id.campoInmueble_arrendatario)).getText().toString();
@@ -89,27 +140,9 @@ public class InsertActivityEmergencia extends ActionBarActivity {
         }
 
     }
-
-
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
 //        getMenuInflater().inflate(R.menu.menu_insert, menu);
         return false;
     }
-
-
-    //    @Override
-//    public boolean onOptionsItemSelected(MenuItem item) {
-//        // Handle action bar item clicks here. The action bar will
-//        // automatically handle clicks on the Home/Up button, so long
-//        // as you specify a parent activity in AndroidManifest.xml.
-//        int id = item.getItemId();
-//
-//        //noinspection SimplifiableIfStatement
-//        if (id == R.id.action_settings) {
-//            return true;
-//        }
-//
-//        return super.onOptionsItemSelected(item);
-//    }
 }
