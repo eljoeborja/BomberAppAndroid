36802a656c79ec8dd300707ee98fa9077af96b2a
gurzaf <gurzaf@gmail.com>
2015-03-07 21:57:54 -0500

    Cambios para actualizar los datos de los vehiculos en el servidor

---
+++ app/src/main/java/com/s4/demodb/rest/ActualizaVehiculo.java
@@ -0,0 +1,102 @@
+package com.s4.demodb.rest;
+
+import android.database.sqlite.SQLiteDatabase;
+import android.os.AsyncTask;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.gson.Gson;
+import com.s4.demodb.R;
+import com.s4.demodb.db.UsuariosDBOpenHelper;
+import com.s4.demodb.db.util.Vehiculo;
+import com.s4.demodb.rest.auxi.RestHelper;
+
+import java.util.List;
+
+public class ActualizaVehiculo extends ActionBarActivity {
+
+    private TextView view;
+    private UsuariosDBOpenHelper dbOpenHelper;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_actualiza_vehiculo);
+
+        view = (TextView) findViewById(R.id.label_json);
+        dbOpenHelper = UsuariosDBOpenHelper.getInstance(this);
+
+        new HttpAsyncTask().execute("");
+
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_actualiza_vehiculo, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    private class HttpAsyncTask extends AsyncTask<String, Void, Boolean> {
+
+        private List<Vehiculo> vehiculos;
+
+        @Override
+        protected Boolean doInBackground(String... objects) {
+
+            SQLiteDatabase db = dbOpenHelper.getReadableDatabase();
+            vehiculos = Vehiculo.getList(db,false);
+
+            final String json = new Gson().toJson(vehiculos);
+
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    view.setText(json);
+                }
+            });
+
+
+            return RestHelper.POST("http://192.168.1.10:8080/BomberApp/ws/vehiculo", json);
+        }
+
+        // onPostExecute displays the results of the AsyncTask.
+        @Override
+        protected void onPostExecute(Boolean result) {
+            String mensaje = "Data not sent!";
+            if(result){
+                mensaje = "Data Sent!";
+
+                SQLiteDatabase db = dbOpenHelper.getWritableDatabase();
+
+                for (Vehiculo vehiculo: vehiculos){
+                    vehiculo.setVehEstado(1);
+                    vehiculo.update(db);
+                }
+
+            }
+            Toast.makeText(getBaseContext(), mensaje, Toast.LENGTH_LONG).show();
+        }
+    }
+
+}
---
+++ app/src/main/java/com/s4/demodb/rest/auxi/RestHelper.java
@@ -0,0 +1,59 @@
+package com.s4.demodb.rest.auxi;
+
+import android.util.Log;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.HttpStatus;
+import org.apache.http.StatusLine;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.DefaultHttpClient;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+/**
+ * Created by gurzaf on 3/7/15.
+ */
+public class RestHelper {
+
+    public static boolean POST(String url, String content){
+        InputStream inputStream = null;
+        String result = "";
+        try {
+
+            // 1. create HttpClient
+            HttpClient httpclient = new DefaultHttpClient();
+
+            // 2. make POST request to the given URL
+            HttpPost httpPost = new HttpPost(url);
+
+
+            StringEntity se = new StringEntity(content);
+
+            // 6. set httpPost Entity
+            httpPost.setEntity(se);
+
+            // 7. Set some headers to inform server about the type of the content
+            httpPost.setHeader("Accept", "application/json");
+            httpPost.setHeader("Content-type", "application/json");
+
+            // 8. Execute POST request to the given URL
+            HttpResponse httpResponse = httpclient.execute(httpPost);
+
+            if(httpResponse.getStatusLine().getStatusCode()== HttpStatus.SC_OK){
+                return true;
+            }
+
+        } catch (Exception e) {
+            Log.d("InputStream", e.getLocalizedMessage());
+        }
+
+        // 11. return result
+        return false;
+    }
+
+}
